47e4f6cdb2136a5795b17dbe43c9479b0a29a583
github username <github email address>
2019-01-10 12:02:10 -0500

    can create new user using JWT

--- app/controllers/api/v1/coaches_controller.rb
+++ app/controllers/api/v1/coaches_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::CoachesController < ApplicationController
+
+  skip_before_action :authorized, only: [:create]
+
   def create
     @coach = Coach.create(coach_params)
     if @coach.valid?
-      render json: {coach: CoachSerializer.new(@coach)}, status: :created
+      @token = encode_token(coach_id: @coach.id)
+      render json: {coach: CoachSerializer.new(@coach), jwt: @token}, status: :created
     else
       render json: {error: 'failed to create user'}, status: :not_acceptable
     end
@@ -10,6 +14,6 @@ class Api::V1::CoachesController < ApplicationController
 
   private
   def coach_params
-    params.require(:coaches).permit(:username, :password_digest, :email, :name)
+    params.require(:coaches).permit(:username, :password, :email, :name)
   end
 end
