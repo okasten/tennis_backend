6126f085d554688988258ae397b1fb424be64294
github username <github email address>
2019-01-15 10:30:04 -0500

    starting a conversation now creates the message as well

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -8,17 +8,25 @@ class Api::V1::ConversationsController < ApplicationController
   end
 
   def create
-
     if @user.is_a? Coach
-      @receiver = Player.find_by(name: find_to[:to])
+      @receiver = Player.find(find_to[:to])
       @conversation = Conversation.create(coach: @user, player: @receiver)
     else
-      @receiver = Coach.find_by(name: find_to[:to])
+      @receiver = Coach.find(find_to[:to])
       @conversation = Conversation.create(player: @user, coach: @receiver)
     end
+
+    @message = Message.create(to: @receiver, from: @user, subject: message_params[:subject], content: message_params[:content], conversation: @conversation, read: false)
+
     render json: @conversation
   end
+
+
   private
+
+  def message_params
+    params.require(:message).permit(:to, :subject, :content)
+  end
 
   def find_to
     params.require(:message).permit(:to)
@@ -28,7 +36,7 @@ class Api::V1::ConversationsController < ApplicationController
 
   def find_user
     if params[:coach_id]
-      @user = Coach.find(params[:player_id])
+      @user = Coach.find(params[:coach_id])
     elsif params[:player_id]
       @user = Player.find(params[:player_id])
     end
