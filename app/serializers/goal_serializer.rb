d22859e00f23b40bb1cab984297e06fdf4cceee4
github username <github email address>
2019-01-18 15:32:45 -0500

    added updated_at to goal serializer

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,4 +1,4 @@
 class GoalSerializer < ActiveModel::Serializer
-  attributes :id, :objective, :met, :player, :kind, :dateMet, :notes, :created_at
+  attributes :id, :objective, :met, :player, :kind, :dateMet, :notes, :created_at, :updated_at
   belongs_to :player
 end
